5b7c7d09d52ba6ebf285704865fd1c50c44f7d89
TejashwiniV <51438156+TejashwiniV@users.noreply.github.com>
2019-08-17 10:34:30 +0530

    Create Assignment1.cpp

---
+++ Assignment1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int sqrt_of_num(int num)
+{
+	int result;
+	int start = 1;
+	int end = num/2;
+	
+	if (num <= 2)
+		return num;
+		
+	while (start <= end)
+	{
+		
+		int mid = (start + end) / 2;
+		long sqr = mid*mid;
+
+		
+		if (sqr == num)
+			return mid;
+
+		
+		else if (sqr < num)
+		{
+			start = mid + 1;
+			result = mid;
+		}
+
+		else
+		{
+			end = mid - 1;
+		}
+	}
+
+	return result;
+}
+
+int main()
+{
+	int num;
+	int temp=0;
+	
+	double time;
+	clock_t start,end;
+	
+	start=clock();
+	cout << "Enter the number to find the square root\n";
+	cin >> num;
+	temp=sqrt_of_num(num);
+	cout << "Square root of a given number "<< num << " is " << temp << endl;
+	end=clock();
+	
+	time=(double)(end-start)/CLOCKS_PER_SEC;
+	cout << "Time complexity " << time << endl;
+	
+	return 0;
+}
