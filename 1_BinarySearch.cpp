0acef78b223fdd0419c94a1ff5f592c967ae51a3
TejashwiniV <51438156+TejashwiniV@users.noreply.github.com>
2019-09-03 21:29:19 +0530

    Update and rename Assignment1.cpp to 1_BinarySearch.cpp

--- 1_BinarySearch.cpp
+++ 1_BinarySearch.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
 #include <time.h>
-
 using namespace std;
-
 int sqrt_of_num(int num)
 {
 	int result;
 	int start = 1;
 	int end = num/2;
-	
 	if (num <= 2)
 		return num;
-		
 	while (start <= end)
 	{
-		
 		int mid = (start + end) / 2;
 		long sqr = mid*mid;
-
-		
 		if (sqr == num)
 			return mid;
-
-		
 		else if (sqr < num)
 		{
 			start = mid + 1;
 			result = mid;
 		}
-
 		else
 		{
 			end = mid - 1;
 		}
 	}
-
 	return result;
 }
 
@@ -42,19 +31,15 @@ int main()
 {
 	int num;
 	int temp=0;
-	
 	double time;
 	clock_t start,end;
-	
 	start=clock();
-	cout << "Enter the number to find the square root\n";
+	cout << "Enter the number to find the square root:";
 	cin >> num;
 	temp=sqrt_of_num(num);
-	cout << "Square root of a given number "<< num << " is " << temp << endl;
+	cout << "Square root of a given number "<< num << " is:" << temp << endl;
 	end=clock();
-	
 	time=(double)(end-start)/CLOCKS_PER_SEC;
-	cout << "Time complexity " << time << endl;
-	
+	cout << "Time complexity is:" << time <<"secs"<<endl;
 	return 0;
 }
