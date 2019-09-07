4a9bfae33065a5f89b8509b12c7ba778dd0b5100
TejashwiniV <51438156+TejashwiniV@users.noreply.github.com>
2019-09-07 10:02:05 +0530

    Create 4a_Dfs.cpp

---
+++ 4a_Dfs.cpp
@@ -0,0 +1,47 @@
+/*Program based on Depth First search
+4a. Given an undirected graph, print all connected components line by line.*/
+
+#include <iostream>
+using namespace std;
+
+int adjacent[10][10],visited[10],n;
+
+void DFS(int v)
+{
+	cout << v+1 <<" ";
+	visited[v]=1;
+	for(int i=0;i<n;i++)
+	{
+		if(adjacent[v][i]==1 && visited[i]==0)
+		{
+			DFS(i);
+		}
+	}
+}
+
+int main()
+{
+	int i,j;
+	cout << "Enter the number of components: ";
+	cin >> n;
+	for (i=0;i<n;i++)
+	{
+		visited[i]=0;
+	}
+	cout << "Enter the adjacent matrix of graph: \n";
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			cin >> adjacent[i][j];
+		}
+	}
+	cout << "DFS traversal: ";
+	for(i=0;i<n;i++)
+	{
+		if(visited[i] == 0)
+			DFS(i);
+	}
+	return 0;
+}
+
